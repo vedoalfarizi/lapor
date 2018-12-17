53bc7dc4eb84da0550203734d12265bd91c2ada1
vedoalfarizi <vedoalfarizi@gmail.com>
2018-12-17 19:13:53 +0700

    berhasil mengambil gambar, namun gagal di forResult

--- app/src/main/java/net/husnilkamil/lapor/InsertActivity.java
+++ app/src/main/java/net/husnilkamil/lapor/InsertActivity.java
@@ -9,20 +9,31 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -33,11 +44,20 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class InsertActivity extends AppCompatActivity{
+public class InsertActivity extends AppCompatActivity implements View.OnClickListener {
     private TextInputLayout textInputJudul;
     private TextInputLayout textInputPelapor;
     private TextInputLayout textInputUraian;
-    private TextInputLayout textInputFoto;
+    private Button btnChooseFoto;
+    private ImageView imageLaporan;
+    private String imageLaporanLoc = "";
+    private static final int CAMERA_PIC_REQUEST = 1111;
+    private String postPath;
+    private Uri fileUri;
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+    private static final String TAG = InsertActivity.class.getSimpleName();
 
     private LocationManager locationManager;
     private LocationListener locationListener;
@@ -57,7 +77,20 @@ public class InsertActivity extends AppCompatActivity{
         textInputJudul = findViewById(R.id.textInputJudul);
         textInputPelapor = findViewById(R.id.textInputPelapor);
         textInputUraian = findViewById(R.id.textInputUraian);
-        textInputFoto = findViewById(R.id.textInputFoto);
+        btnChooseFoto = (Button) findViewById(R.id.buttonFoto);
+        imageLaporan = (ImageView) findViewById(R.id.imageLaporan);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            btnChooseFoto.setEnabled(false);
+            requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 10);
+            return;
+        }else{
+            btnChooseFoto.setEnabled(true);
+        }
+
+        btnChooseFoto.setOnClickListener(this);
 
         geocoder = new Geocoder(this, Locale.getDefault());
 
@@ -100,13 +133,133 @@ public class InsertActivity extends AppCompatActivity{
             requestPermissions(new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.INTERNET
-            }, 10);
+            }, 0);
 
             return;
         }
 
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                btnChooseFoto.setEnabled(true);
+            }
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonFoto :
+                dispatchPictureTakerAction();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == CAMERA_PIC_REQUEST){
+                if(Build.VERSION.SDK_INT > 21){
+                    Glide.with(this).load(imageLaporanLoc).into(imageLaporan);
+                    postPath = imageLaporanLoc;
+                    Toast.makeText(this, "masuk lokasi", Toast.LENGTH_SHORT).show();
+                }else{
+                    Glide.with(this).load(fileUri).into(imageLaporan);
+                    postPath = fileUri.getPath();
+                }
+            }
+        }else{
+            Toast.makeText(this, "gagal masuk act result", Toast.LENGTH_SHORT).show();
+            Log.e("error", "error result");
+        }
+    }
+
+    private void dispatchPictureTakerAction() {
+        if(Build.VERSION.SDK_INT > 21){
+            Intent iTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(iTakePicture.resolveActivity(getPackageManager()) != null){
+                File photo = null;
+
+                photo = createPhotoFile();
+
+                if(photo != null){
+                    Uri outputUri = FileProvider.getUriForFile(
+                            this,
+                            BuildConfig.APPLICATION_ID + ".provider",
+                            photo
+                    );
+                    iTakePicture.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+                    iTakePicture.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//                    Toast.makeText(this, "result akan mulai", Toast.LENGTH_SHORT).show(); ->jalan
+                    startActivityForResult(iTakePicture, CAMERA_PIC_REQUEST);
+                }
+            }
+        }else{
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(intent, CAMERA_PIC_REQUEST);
+        }
+    }
+
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + ".jpg");
+        }  else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private File createPhotoFile() {
+        String nama = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File image = null;
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/fotoLaporan");
+
+        if(!storageDir.exists()){
+            storageDir.mkdir();
+        }
+
+        image = new File(storageDir, nama + ".jpg");
+
+        imageLaporanLoc = image.getAbsolutePath();
+        Toast.makeText(this, "gambar sudah dibuat", Toast.LENGTH_SHORT).show();
+
+        return image;
     }
 
     private boolean validateJudul(){
@@ -146,13 +299,10 @@ public class InsertActivity extends AppCompatActivity{
     }
 
     private boolean validateFoto(){
-        String fotoInput = textInputFoto.getEditText().getText().toString().trim();
-
-        if(fotoInput.isEmpty()){
-            textInputFoto.setError("Foto tidak boleh kosong");
+        if(postPath == null || postPath == ""){
+            Toast.makeText(this, "Foto bukti tidak boleh kosong", Toast.LENGTH_SHORT).show();
             return false;
         }else{
-            textInputFoto.setError(null);
             return true;
         }
     }
@@ -171,7 +321,7 @@ public class InsertActivity extends AppCompatActivity{
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String tanggal = sdf.format(calendar.getTime());
 
-        String foto = textInputFoto.getEditText().getText().toString();
+        File file = new File(postPath);
 
         LaporanApiClient client = (new Retrofit.Builder()
             .baseUrl("http://nagarikapa.com/lapor/api/")
@@ -179,7 +329,7 @@ public class InsertActivity extends AppCompatActivity{
             .build())
                 .create(LaporanApiClient.class);
 
-        Call<ResponseBody> call = client.createLaporan(judul, uraian, pelapor, tanggal, lokasi, foto);
+        Call<ResponseBody> call = client.createLaporan(judul, uraian, pelapor, tanggal, lokasi, file);
 
         call.enqueue(new Callback<ResponseBody>() {
             @Override
@@ -197,5 +347,19 @@ public class InsertActivity extends AppCompatActivity{
                 Toast.makeText(InsertActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelable("file_uri", fileUri);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        fileUri = savedInstanceState.getParcelable("file_uri");
     }
 }
